fe453c9696a4dac1d8c7bdb703f8d74070e9c717
Sergio Behrends <sbehrends@Sergios-MacBook-Pro.local>
2016-10-04 01:25:16 -0300

    Added mongo server

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const app = express();
-// const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient
 
+let db;
 const port = process.env.PORT;
+MongoClient.connect(process.env.MONGO_HOST, (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(port, function () {
+    console.log(`Listening on port ${port}!`);
+  });
+
+  // Some demo content
+  const data = {
+    name: 'Dr. Emmett Brown',
+    text: 'Great Scott!'
+  }
+
+  db.collection('quotes').find({}, { limit: 1 }).toArray(function(err, results) {
+    if (results.length === 0) {
+      db.collection('quotes').save(data, (err, result) => {
+        if (err) return console.log(err)
+        console.log('Saved demo content')
+      })
+    }
+  })
+});
+
+
 
 app.get('/', function (req, res) {
-  res.send('Hello Docker!');
+  db.collection('quotes').find({}, { limit: 1 }).toArray(function(err, results) {
+    if (results.length === 0) {
+      res.send('Hello Docker!');
+      return;
+    }
+    res.send(results[0].text);
+  });
 });
 
-app.listen(port, function () {
-  console.log(`Listening on port ${port}!`);
-});
